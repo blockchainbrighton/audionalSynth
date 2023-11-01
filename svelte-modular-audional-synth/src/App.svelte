4e07cc05732afe2268848d1738c717f6ed1794a9
blockchainbrighton <jim@blockchainbrighton.com>
2023-11-01 18:03:11 +0000

    Updates

--- svelte-modular-audional-synth/src/App.svelte
+++ svelte-modular-audional-synth/src/App.svelte
@@ -1,24 +1,14 @@
 <!-- App.svelte -->
 
 <script>
-
     import Piano from './Piano.svelte';
     import MidiController from './MidiController.svelte';
-    import { midiData } from './midiStore.js'; // Import the midiData store
+    import { midiData } from './midiStore.js';
     import Oscillator from './Oscillator.svelte';
+    import { runMidiTest } from './midiTest.js';
 
     let audioContext = null;
     let gainNode = null;
-
-    function runMidiTest() {
-        // Test MIDI Note On
-        simulateMIDIMessage('noteOn', 60, 100); // Play middle C with velocity 100
-
-        // Wait for 2 seconds and then stop the note
-        setTimeout(() => {
-            simulateMIDIMessage('noteOff', 60, 0);
-        }, 2000);
-    }
 
     function initAudio() {
         audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -30,7 +20,6 @@
         gainNode.connect(audioContext.destination);
         console.log('App: Gain node connected to audio context destination');
 
-        // Optional: Resume audio context in case it's suspended
         audioContext.resume().then(() => {
             console.log('App: Audio context resumed');
         }).catch(err => {
@@ -38,21 +27,17 @@
         });
     }
 
-    // Subscribe to the midiData store
     let midiMessage;
     midiData.subscribe(value => {
         midiMessage = value;
         console.log('App: midiData subscription callback, received value:', value);
     });
-
-    // Expose the test function to the global scope
-    window.runMidiTest = runMidiTest;
 </script>
 
 <main>
     <h1>Audional Synth - Basic 88 note Keyboard</h1>
     <button on:click={initAudio}>Initialize Audio</button>
-    <button on:click={runMidiTest}>Run MIDI Test</button> <!-- Add this button -->
+    <button on:click={runMidiTest}>Run MIDI Test</button>
     {#if audioContext && gainNode}
         <Piano />
         <MidiController />
@@ -60,15 +45,12 @@
     {/if}
 </main>
 
-
-
 <style>
     main {
         text-align: center;
         padding: 1em;
         margin: 0 auto;
     }
-
     h1 {
         color: #ff3e00;
         text-transform: uppercase;
